ebb8d41cf5a9f477fc864323cc825cb999b80a1a
Julienne San Luis <julienne.codes@gmail.com>
2021-02-22 21:56:29 -0500

    converted helper functions to methods

--- src/server/cart.js
+++ src/server/cart.js
@@ -22,22 +22,48 @@ class Cart {
     return this.subtotal + this.tax;
   }
 
+  changeItemQuantity(menuItem, newQty) {
+    const updatedCart = this.cartItems.map((cartItem) => {
+      if (cartItem.name === menuItem.name) {
+        return { ...cartItem, quantity: cartItem.quantity + newQty };
+      } else {
+        return cartItem;
+      }
+    });
+
+    return updatedCart.filter((cartItem) => cartItem.quantity > 0);
+  }
+
+  addItemToCart(menuItem, newQty) {
+    return [...this.cartItems, { ...menuItem, quantity: newQty }];
+  }
+
+  contains(menuItem) {
+    const x = this.cartItems.find((cartItem) => {
+      return cartItem.name === menuItem.name;
+    });
+
+    //   if (x) {return true;} else {return false;}
+    if (x) return true;
+    else return false;
+  }
+
   updateCart(menuItem, newQty) {
-    if (isInCart(this, menuItem.name)) {
-      const updatedCartItems = changeItemQuantity(this, menuItem.name, newQty);
+    if (this.contains(menuItem)) {
+      const updatedCartItems = this.changeItemQuantity(menuItem, newQty);
       return new Cart(updatedCartItems);
     }
 
-    if (isNotInCart(this, menuItem.name) && newQty > 0) {
-      const updatedCartItems = addItemToCart(this, menuItem, newQty);
+    if (!this.contains(menuItem) && newQty > 0) {
+      const updatedCartItems = this.addItemToCart(menuItem, newQty);
       return new Cart(updatedCartItems);
     } else {
       return this;
     }
   }
 
-  removeItem(menuItem){
-    if (isInCart(this, menuItem.name)) {
+  removeItem(menuItem) {
+    if (this.contains(menuItem)) {
       const cartItems = this.cartItems;
       const updatedCartItems = cartItems.filter(
         (cartItem) => !(cartItem.name === menuItem.name)
@@ -47,50 +73,14 @@ class Cart {
       return this;
     }
   }
-  toJSON(){
+  toJSON() {
     return {
       cartItems: this.cartItems,
       subtotal: this.subtotal,
       tax: this.tax,
       total: this.total,
-    }
+    };
   }
-}
-
-function changeItemQuantity(cart, itemName, newQty) {
-  const updatedCart = cart.cartItems.map((cartItem) => {
-    if (cartItem.name === itemName) {
-      return { ...cartItem, quantity: cartItem.quantity + newQty };
-    } else {
-      return cartItem;
-    }
-  });
-
-  return updatedCart.filter((cartItem) => cartItem.quantity > 0);
-}
-
-function addItemToCart(cart, item, newQty) {
-  return [...cart.cartItems, { ...item, quantity: newQty }];
-}
-
-function isInCart(cart, itemName) {
-  const x = cart.cartItems.find((cartItem) => {
-    return cartItem.name === itemName;
-  });
-
-  //   if (x) {return true;} else {return false;}
-  if (x) return true;
-  else return false;
-}
-
-function isNotInCart(cart, itemName) {
-  const x = cart.cartItems.find((cartItem) => {
-    return cartItem.name === itemName;
-  });
-
-  //   if (x) {return false;} else {return true;}
-  if (x) return false;
-  else return true;
 }
 
 exports.Cart = Cart;
